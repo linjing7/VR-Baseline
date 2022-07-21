8f32936dcc520a0c039406ea5a50d392e2afcc07
linjing7 <70081899+linjing7@users.noreply.github.com>
2022-07-21 21:33:05 +0800

    Add files via upload

---
+++ VR_Baseline/mmedit/models/backbones/sr_backbones/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .basicvsr_net import BasicVSRNet
+from .basicvsr_pp import BasicVSRPlusPlus
+from .FGST import FGST
+
+__all__ = ['BasicVSRNet', 'BasicVSRPlusPlus', 'FGST']
