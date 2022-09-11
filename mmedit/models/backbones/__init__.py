7b4865be8c8f01fdd4f584ffe388f61891c16bb6
linjing7 <jinglin.stu@gmail.com>
2022-09-11 10:14:25 +0800

    Update __init__.py

--- mmedit/models/backbones/__init__.py
+++ mmedit/models/backbones/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .sr_backbones import BasicVSRPlusPlus
+from .sr_backbones import FGST, S2SVR
 
-__all__ = ['BasicVSRPlusPlus']
+__all__ = ['FGST', 'S2SVR']
